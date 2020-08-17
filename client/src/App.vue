a63529fd7e6aa6ebedb87e1166f0181180b2c955
gcdhg <gcdhg@outlook.com>
2020-08-17 10:35:21 +0300

    added vue client

---
+++ client/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <ToDoList>
+
+    </ToDoList>
+  </div>
+</template>
+
+<script>
+import ToDoList from './components/ToDoList.vue';
+
+const API_URL = 'http://localhost:3000';
+
+export default {
+  name: 'App',
+  components: {
+    ToDoList,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
